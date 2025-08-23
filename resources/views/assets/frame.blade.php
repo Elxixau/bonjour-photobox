de2b5da92cc1f2fc22b483be575a67e7d89fa7a1
Elxixau <indra90078@gmail.com>
2025-08-23 14:08:26 +0800

    add back button

--- resources/views/assets/frame.blade.php
+++ resources/views/assets/frame.blade.php
@@ -3,6 +3,14 @@
 @section('content')
 <h1 class="text-3xl font-serif font-bold text-center mb-6">Pilih Frame</h1>
 
+<div class="absolute top-4   left-2 z-50 ">
+    <a href="{{ url()->previous() }}" 
+       class="bg-white text-black font-semibold py-2 px-4 rounded-lg border-2 border-black shadow-black shadow-[4px_4px_0_0] hover:shadow-[6px_6px_0_0] transition duration-300 text-sm">
+        Kembali
+    </a>
+</div>
+
+
 @livewire('frame-selector', ['orderCode' => $order->order_code, 'layout' => $layout, 'selectedFrame' => old('frame_id')])
 
 @endsection
