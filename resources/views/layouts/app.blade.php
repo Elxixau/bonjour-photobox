f1e4cb98f351338a74b7f0ce382bc17acb3e89e1
Elxixau <indra90078@gmail.com>
2025-08-25 18:58:13 +0800

    update interface 0.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,46 +15,46 @@
 </head>
 <body >
 
-<div class="min-h-screen flex flex-col justify-center items-center bg-gray-100" style="background-image: url('{{ asset('image/bgBonjour.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat;" >
+<div class="min-h-screen flex flex-col justify-center items-center " style="background-image: url('{{ asset('image/bgBonjour.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat;" >
     <div class=" x-4">
         
     {{-- Navbar Elevated --}}
     <nav
-        class="relative rounded-lg border-2 border-black bg-white px-6 py-10 mb-10 w-full max-w-6xl 
+        class="relative rounded-lg border-2 border-black bg-red-950 px-6 py-10 mb-10 w-full max-w-6xl 
         shadow-black shadow-[8px_8px_0_0] flex items-center flex-wrap gap-6 min-h-[100px]"
     >
         {{-- Logo kiri --}}
-        <div class="relative lg:text-7xl text-5xl px-2 md:px-2 py-3 font-black md:mr-2 mr-8 font-serif">
+        <div class="relative text-white lg:text-7xl text-5xl px-2 md:px-2 py-3 font-black md:mr-2 mr-8 font-serif">
             Bonjour
         </div>
 
         {{-- Menu label statis dengan kondisi aktif --}}
         <a href="{{ route('panduan') }}" 
-        class="px-2  py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
+        class="px-2 text-white py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
                 {{ request()->routeIs('panduan') ? 'bg-black text-white' : '' }}">
             Panduan
         </a>
 
         <a href="{{ route('payment.index') }}" 
-        class="px-2  py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
+        class="px-2 text-white py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
                 {{ request()->routeIs('payment.*') ? 'bg-black text-white' : '' }}">
             Pembayaran
         </a>
 
         <a href="{{ route('frame.choose', ['orderCode' => 1, 'layout' => 'default']) }}" 
-        class="px-2  py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
+        class="px-2 text-white py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
                 {{ request()->routeIs('frame.*') ? 'bg-black text-white' : '' }}">
             Pilih Frame
         </a>
 
         <a href="{{ route('sesi-foto.show', ['orderCode' => 1]) }}" 
-        class="px-2  py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
+        class="px-2 text-white py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
                 {{ request()->routeIs('sesi-foto.*') ? 'bg-black text-white' : '' }}">
             Sesi Foto
         </a>
 
         <a href="{{ route('filter.index', ['orderCode' => 1]) }}" 
-        class="px-2  py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
+        class="px-2 text-white py-2 md:text-md text-2xl font-reguler rounded whitespace-nowrap
                 {{ request()->routeIs('filter.*') ? 'bg-black text-white' : '' }}">
             Edit Foto
         </a>
@@ -64,7 +64,7 @@
 
     {{-- Section Elevated --}}
     <section
-        class="relative  border-2 border-black bg-white px-12 py-14 max-w-6xl w-full 
+        class="relative text-white  border-2 border-black bg-red-950 px-12 py-14 max-w-6xl w-full 
         shadow-black shadow-[10px_10px_0_0] hover:shadow-[12px_12px_0_0] 
         transition-all flex flex-col items-center"     
     >
--- resources/views/pages/sesi_foto.blade.php
+++ resources/views/pages/sesi_foto.blade.php
@@ -24,14 +24,14 @@
 </h1>
 
 <div class="absolute top-4 right-4 z-50 flex gap-2">
-    <div class="relative rounded-lg border-2 border-black bg-white px-3 py-1 shadow-black shadow-[4px_4px_0_0] flex items-center gap-2">
+    <div class="pointer-events-none relative rounded-lg border-2 border-black bg-white px-3 py-1 shadow-black shadow-[4px_4px_0_0] flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6l4 2m6-2a10 10 0 11-20 0 10 10 0 0120 0z" />
         </svg>
         <span id="globalTimer" class="text-lg font-extrabold text-gray-600 select-none">--:--</span>
     </div>
 
-    <button id="orientationBtn" class="px-3 py-1 bg-gray-900 text-white rounded-md hover:bg-gray-700 transition flex items-center gap-1">
+    <button id="orientationBtn" class=" pointer-events-none px-3 py-1 bg-gray-900 text-white rounded-md hover:bg-gray-700 transition flex items-center gap-1">
         <svg id="orientationIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 24 24">
             <rect x="6" y="4" width="12" height="16" rx="2" ry="2" stroke="currentColor" stroke-width="2" fill="none"/>
         </svg>
