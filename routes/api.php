029e7b48abde2ace48241d131492ce0c4d30e55c
Elxixau <indra90078@gmail.com>
2025-09-09 20:24:00 +0800

    add API

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\DigicamController;
+use App\Http\Controllers\Api\KategoriController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,6 +16,8 @@ Route::post('/callback', [PaymentController::class, 'callback']);
 
 Route::post('/upload-photo', [DigicamController::class, 'uploadPhoto']);
 Route::post('/upload-print', [DigicamController::class, 'uploadPrint']);
-
 Route::post('/delete-photo', [DigicamController::class, 'deletePhoto']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori/{$id}', [KategoriController::class, 'show']);
 
